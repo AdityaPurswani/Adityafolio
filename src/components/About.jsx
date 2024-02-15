1a45cb2c1cd3cc1fb1a4ed918f726425db7a20bf
AdityaPurswani <adityapurswani9621@gmail.com>
2024-02-15 02:38:19 +0000

    initial commit

---
+++ src/components/About.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Tilt} from 'react-tilt'
+import {motion} from 'framer-motion'
+import {styles} from '../styles'
+import {services} from '../constants'
+import {fadeIn, textVariant} from '../utils/motion'
+
+const About = () => {
+  return (
+
+    <>
+      <motion.dev>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
+      </motion.dev>
+    </>
+
+  )
+}
+
+export default About;
---
+++ src/components/Tech.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Tech = () => {
+  return (
+    <div className='bg-primary'>Tech</div>
+  )
+}
+
+export default Tech
